fb6045ad46a0ac90413e18a55fab25d07edec1a1
martinoferrero1 <martinoferrero02@gmail.com>
2024-12-16 17:15:39 -0300

    fix(golden_and_death_cross): correccion de la logica de verificacion de las condiciones de compra y venta, quitando parte de las condiciones innecesarias. Tambien se modifico las sma individuales por dos diccionarios de smas, con una sma para cada activo

--- Strategies/golden_and_death_cross.py
+++ Strategies/golden_and_death_cross.py
@@ -9,9 +9,8 @@ class GoldenDeathCross(GeneralStrategy):
     Cross, lo que indica una posible tendencia alcista y se compra.
 
     Atributos:
-        params (tuple): Parámetros de la estrategia. Incluye el período de las SMA cortas y largas.
-        sma_short_period (bt.indicators.SimpleMovingAverage): SMA para el período corto.
-        sma_long_period (bt.indicators.SimpleMovingAverage): SMA para el período largo.
+        smas_short_period (dict[bt.feeds.Data, bt.indicators.SimpleMovingAverage]): SMAs para el período corto (una por cada datafeed).
+        smas_long_period (dict[bt.feeds.Data, bt.indicators.SimpleMovingAverage]): SMAs para el período largo (una por cada datafeed).
 
     Args:
         short_period (int): El número de períodos para calcular la SMA corta, siendo que su valor por defecto es de 50.
@@ -19,44 +18,61 @@ class GoldenDeathCross(GeneralStrategy):
     """
 
     params = (
-        ('short_period', 50),
-        ('long_period', 200),
+        ('short_period', 10),
+        ('long_period', 30),
     )
 
     def __init__(self):
         """
-        Para inicializar la estrategia y calcula las SMA corta y larga utilizando los períodos proporcionados
+        Para inicializar la estrategia y calcula las SMAs cortas y largas utilizando los períodos proporcionados
         junto con el precio de cierre de los datos.
         """
         super().__init__()
-        self.sma_short_period = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.short_period)
-        self.sma_long_period = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.long_period)
+
+        self.smas_short_period = {}
+        self.smas_long_period = {}
+        
+        for data in self.datas:
+            self.smas_short_period[data._name] = bt.indicators.SimpleMovingAverage(data.close, period=self.params.short_period)
+            self.smas_long_period[data._name] = bt.indicators.SimpleMovingAverage(data.close, period=self.params.long_period)
 
     
-    def conditions_buy(self):
+    def conditions_buy(self, data) -> bool:
         """
         Método que define las condiciones para ejecutar una orden de compra (implementación del método de la clase genérica de estrategia).
 
         La orden de compra se genera cuando la SMA de corto plazo cruza hacia arriba la de largo plazo (Golden Cross).
 
+        Args:
+            data (bt.feeds.YahooFinanceCSVData): Objeto de datos de Yahoo Finance con la información del activo correspondiente.
+
         Returns:
             bool: Verdadero si la SMA de corto plazo cruza hacia arriba la de largo plazo, lo que indica una señal de compra (por posible tendencia alcista).
         """
 
-        return self.sma_short_period[0] > self.sma_long_period[0] and self.sma_short_period[-1] < self.sma_long_period[-1] # Están ambas condiciones porque en realidad no solo debe ser mayor la SMA corta en el momento actual, sino que también debe haber sido menor en el candlestick previo, lo que indica que hubo un cruce hacia arriba.
+        return self.smas_short_period[data._name][0] > self.smas_long_period[data._name][0] # No está la segunda condición porque en el contexto del problema
+                                                                                            # no se puede estar comprado y seguir comprando ya que antes se
+                                                                                            # verifica que no haya posición, por lo que está implícito
+                                                                                            # que en el candlestick previo la SMA corta no era mayor que la SMA larga.
     
-    def conditions_sell(self):
+    def conditions_sell(self, data) -> bool:
         """
         Método que define las condiciones para ejecutar una orden de venta (implementación del método de la clase genérica de estrategia).
 
         La orden de venta se genera cuando la SMA de corto plazo cruza hacia abajo la de largo plazo (Death Cross).
 
+        Args:
+            data (bt.feeds.YahooFinanceCSVData): Objeto de datos de Yahoo Finance con la información del activo correspondiente.
+
         Returns:
             bool: Verdadero si la SMA de corto plazo cruza hacia abajo la de largo plazo, lo que indica una señal de venta (por posible tendencia bajista).
         """
-        return self.sma_short_period[0] < self.sma_long_period[0] and self.sma_short_period[-1] > self.sma_long_period[-1] # Están ambas condiciones porque en realidad no solo debe ser menor la SMA corta en el momento actual, sino que también debe haber sido mayor en el candlestick previo, lo que indica que hubo un cruce hacia abajo.
+        return self.smas_short_period[data._name][0] < self.smas_long_period[data._name][0] # No está la segunda condición porque en el contexto del problema
+                                                                                            # no se puede estar vendido y seguir vendiendo ya que antes se
+                                                                                            # verifica que haya una posición (que además no puede ser menor a 0),
+                                                                                            # por lo que está implícito que en el candlestick previo la SMA corta no era menor que la SMA larga. 
     
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Devuelve una representación en cadena de la estrategia, mostrando los períodos de las SMA corta y larga utilizadas.
 
