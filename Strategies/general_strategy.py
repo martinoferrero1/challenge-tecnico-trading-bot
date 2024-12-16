b9e873ca2ddab83e9e91d4f856dbbb2c5aa602a6
martinoferrero1 <martinoferrero02@gmail.com>
2024-12-16 17:15:06 -0300

    actualizacion de la logica para las distintas ordenes, incorporacion de monto de operaciones de compra pendientes y escritura sobre archivo de logs

--- Strategies/general_strategy.py
+++ Strategies/general_strategy.py
@@ -5,22 +5,37 @@ class GeneralStrategy(bt.Strategy):
     Clase base para estrategias concretas.
     Permite gestionar compras y ventas automáticas basadas en condiciones definidas.
 
-    Attributes: asset_registry (dict): Registro de activos y sus respectivas cantidades para la estrategia, para saber cuántos activos se tienen en cartera disponibles para vender con la estrategia.
+    Atributos de clase:
+        pending_operation_funds (int): Monto de dinero pendiente de operaciones, para evitar que una operación de compra utilice fondos de otra operación de compra que todavía no se ha completado.
 
-    Args: investment_fraction (float): Fracción del portafolio a invertir en cada activo. Por defecto es 0.1, o sea el 10% del portafolio.
+    Atributos:
+        log_file (file): Archivo de log para registrar las operaciones realizadas, se guarda en la carpeta logs del proyecto.
+        assets_registry (dict): Registro de activos y sus respectivas cantidades para la estrategia, para saber cuántos activos se tienen en cartera disponibles para vender con la estrategia.
+    Args:
+        investment_fraction (float): Fracción del portafolio a invertir en cada activo. Por defecto es 0.1, o sea el 10% del portafolio. 
+        log_file_path (str): Ruta al archivo de log. Por defecto es 'logs/operations.log'.
     """
 
     params = (
         ('investment_fraction', 0.1),
+        ('log_file_path', 'logs/operations.log'),
     )
+
+    pending_operation_funds = 0
 
     def __init__(self):
         """
-        Este método inicializa los atributos y registros necesarios para la estrategia.
-        """    
-        super().__init__()
-        self.asset_registry = {} # Si bien podria ser un atributo de clase, cada instancia de estrategia solo deberia conocer sus
-                                # propias cantidades de activos. No hay problema con esto porque no tendria sentido pasar al cerebro dos estrategias del mismo tipo con iguales indicadores, por lo que cada una es diferente y no se mezclan.
+        Inicializa la estrategia y el registro de activos con 0 unidades de cada uno. Además, abre el archivo de log para registrar
+        las operaciones.
+        """
+        self.assets_registry = {}
+        for data in self.datas:
+            self.assets_registry[data._name] = 0    # Si bien podria ser un atributo de clase, cada instancia de estrategia solo deberia conocer sus
+                                                    # propias cantidades de activos. No hay problema con esto porque no tendria sentido pasar al cerebro dos estrategias del mismo tipo con iguales indicadores, por lo que cada una es diferente y no se mezclan.
+
+        self.log_file = open(self.params.log_file_path, 'a', buffering=1)   # A partir de setear el buffering en 1 se asegura que el archivo se
+                                                                            # escriba en cada operación en orden, lo cual es crucial ya que sino pueden ocurrir problemas en el orden
+                                                                            # de los registros (y es más eficiente al no hacer un flush en cada escritura)
         
     def add_log_entry(self, txt, dt=None):
         """
@@ -28,40 +43,51 @@ class GeneralStrategy(bt.Strategy):
 
         Args:
             txt (str): Mensaje a registrar.
-            dt (datetime, opcional): Fecha asociada al mensaje.
+            dt (datetime): Fecha asociada al mensaje.
         """
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
+        if dt is None:
+            dt = self.datas[0].datetime.date(0) # Para que si no se pasa una fecha, se utilice la del candlestick actual del primer datafeed
+        if self.log_file:
+            self.log_file.write(f"{dt.isoformat()}, {txt}" + '\n')
         
-    def get_purchase_vol(self):
+    def get_purchase_vol(self, data) -> int:
         """
-        Este método calcula el tamaño de la posición a comprar basado en la fracción del portafolio.
+        Este método calcula el tamaño de la posición a comprar basado en la fracción del portafolio que se puede utilizar.
+
+        Args:
+            data (bt.feeds.YahooFinanceCSVData): Objeto de datos de Yahoo Finance con la información del activo correspondiente.
 
         Returns:
             int: Tamaño de la posición a comprar.
         """
         total_value = self.broker.getvalue()
         money_to_invest = total_value * self.params.investment_fraction
+        available_funds = self.broker.get_cash() - GeneralStrategy.pending_operation_funds
 
-        if money_to_invest <= self.broker.get_cash():
-            size = int(money_to_invest / self.data.close[0])
-            if size > 0:
-                return size
+        if money_to_invest <= available_funds:
+            size = int(money_to_invest / data.close[0])
+            return size
         
-        return 0
+        return 0 # Si bien podría quitarse el if y retornar directamente el resultado de la división porque el money_to_invest en este contexto no debiera ser nunca negativo, no está de más el chequeo
     
-    def conditions_buy(self):
+    def conditions_buy(self, data) -> bool:
         """
         Método que define las condiciones para ejecutar una orden de compra (debe ser implementado por las clases hijas).
+
+        Args:
+            data (bt.feeds.YahooFinanceCSVData): Objeto de datos de Yahoo Finance con la información del activo correspondiente.
 
         Returns:
             bool: Verdadero si se cumplen las condiciones de compra.
         """
         return True
     
-    def conditions_sell(self):
+    def conditions_sell(self, data) -> bool:
         """
         Método que define las condiciones para ejecutar una orden de venta (debe ser implementado por las clases hijas).
+
+        Args:
+            data (bt.feeds.YahooFinanceCSVData): Objeto de datos de Yahoo Finance con la información del activo correspondiente.
 
         Returns:
             bool: Verdadero si se cumplen las condiciones de venta.
@@ -70,46 +96,33 @@ class GeneralStrategy(bt.Strategy):
         
     def next(self):
         """
-        Este método es la lógica principal de la estrategia, ejecutado en cada paso del datafeed. En caso de que se cumpla con las
-        condiciones y el volumen a comprar sea mayor a 0, se genera la orden siempre y cuando sea por un volumen mayor a 0,
-        sumando la cantidad correspondiente al registro de ese activo en la estrategia. Para las ventas debe cumplirse tanto las
-        condiciones propias de la estrategia como que la cantidad de activos a vender sea menor o igual a la cantidad disponible
-        en el registro de activos de la instancia de la misma (de esta forma solo se pueden vender activos comprados a traves de la misma estrategia).
+        Este método es la lógica principal de la estrategia, ejecutado en cada paso de cada datafeed. En caso de que no se cuente
+        con una posición (cuando es de 0 en el assets_registry para ese activo) y se cumpla con las condiciones específicas de la
+        estrategia, se genera la orden siempre y cuando sea por un volumen mayor a 0. Para las ventas debe cumplirse tanto las
+        condiciones propias de la estrategia como que se tenga una posición, y se vende tanto como la misma indique a través del
+        assets_registry para ese activo. Se actualiza si corresponde el monto de fondos pendientes de operaciones de compra.
+        
+        IMPORTANTE: De esta forma solo se pueden vender activos comprados a través de la misma estrategia, porque no se puede
+        vender si no hay posición, y tampoco podría ser negativa ya que se vende lo que se tiene en posición para ese activo en la estrategia.
         """
-        if not self.position and self.conditions_buy():
-            vol = self.get_purchase_vol()
-            if vol > 0:
-                self.add_log_entry('ORDEN DE COMPRA GENERADA, PRECIO: %.2f, CANTIDAD: %i' % 
-                        (self.datas[0].close, vol))
-                self.buy(size=vol)
+        for data in self.datas:
+            position_data = self.assets_registry.get(data._name)
+            if position_data == 0 and self.conditions_buy(data):
+                vol = self.get_purchase_vol(data)
+                if vol > 0:
+                    GeneralStrategy.pending_operation_funds += vol * data.close[0] # Se reserva el dinero para la operación recién acá porque antes aún no se sabe si se va a generar la orden
+                    self.add_log_entry('ORDEN DE COMPRA GENERADA, ACTIVO: %s, PRECIO: %.2f, CANTIDAD: %i' % 
+                            (data._name, data.close[0], vol), self.datas[0].datetime.date(0))
+                    self.buy(data=data._name, size=vol)
 
-                self.asset_registry[self.data._name] = self.asset_registry.get(self.data._name, 0) + vol
+            elif position_data > 0 and self.conditions_sell(data): # La verificación de posición ya impide que se venda si es 0, o sea que va a haber que comprar primero, y nunca se venderá algo que no se tiene
+                self.add_log_entry('ORDEN DE VENTA GENERADA, ACTIVO: %s, PRECIO: %.2f, CANTIDAD: %i' % 
+                        (data._name, data.close[0], position_data), self.datas[0].datetime.date(0))
+                self.sell(data=data._name, size=position_data)  # Al vender lo que indica la posición respecto al activo en sí, que es
+                                                                # independiente para cada estrategia, ya se impide que quede en una posición
+                                                                # negativa, cumpliendo con la restricción de que se vende a partir de la misma estrategia de compra y que solo se vende lo que se tiene,
+                                                                # porque cada estrategia vende lo que tiene en su posición
 
-        elif self.position and self.can_sell(self.data._name, self.position_size) and self.conditions_sell():
-            self.add_log_entry('ORDEN DE VENTA GENERADA, PRECIO: %.2f, CANTIDAD: %i' % 
-                    (self.datas[0].close, self.position_size))
-            self.sell(size=self.position_size)
-
-            self.asset_registry[self.data._name] -= self.position_size
-            if self.asset_registry[self.data._name] == 0: # Acá se pregunta si la cantidad de activos es igual a 0 porque no puede ser negativo, ya que el metodo can_sell se encarga de verificar si la cantidad de activos es mayor o igual a la cantidad que se quiere vender
-                del self.asset_registry[self.data._name]
-
-    def can_sell(self, asset_name, vol_to_sell):
-        """
-        Este método verifica si se puede vender un volumen específico de un activo, en base a la cantidad de activos que se
-        adquirieron y vendieron a través de la estrategia.
-
-        Args:
-            asset_name (str): Nombre del activo.
-            vol_to_sell (int): Volumen a vender.
-
-        Returns:
-            bool: Verdadero si se puede vender el volumen solicitado.
-        """
-        if self.asset_registry.get(asset_name, 0) >= vol_to_sell:
-            return True
-        return False
-    
     def notify_order(self, order):
         """
         Verifica sobre el estado de las órdenes y maneja sus resultados. Finalmente la orden se establece en None
@@ -132,41 +145,49 @@ class GeneralStrategy(bt.Strategy):
             
     def handle_completed_order(self, order):
         """
-        Este método maneja órdenes completadas, actualizando registros y agregando logs para las mismas.
+        Este método maneja órdenes completadas, actualizando tanto el assets_registry como el fondo destinado a operaciones
+        pendientes, según corresponda. También agrega logs para dichas órdenes.
 
         Args:
             order (Order): Objeto de orden completada.
         """
         if order.isbuy():
-            self.add_log_entry('COMPRA EJECUTADA, ID: %i, ACTIVO: %s, PRECIO: %.2f, COSTO: %.2f, CANTIDAD: %i, %.2f%% DE LA CARTERA UTILIZADO' % 
-                (order.ref, order.data._name, order.executed.price, order.executed.value, order.executed.size, self.params.investment_fraction * 100))
-            
-            self.positions_strategy[order.data._name] += order.executed.size
-        
-        elif order.issell():
-            self.add_log_entry('VENTA EJECUTADA, ID: %i, ACTIVO: %s, PRECIO: %.2f, COSTO: %.2f, CANTIDAD: %i' % 
-                (order.ref, order.data._name, order.executed.price, order.executed.value, order.executed.size))
+            GeneralStrategy.pending_operation_funds -= order.executed.size * order.data.close[0] # Al haberse completado la orden de compra se restan los fondos reservados para la misma
+            self.add_log_entry('COMPRA EJECUTADA CON ESTRATEGIA DE %s, ID: %i, ACTIVO: %s, PRECIO: %.2f, COSTO: %.2f, CANTIDAD: %i, %.2f%% DE LA CARTERA UTILIZADO' % 
+                (str(self), order.ref, order.data._name, order.executed.price, order.executed.value, order.executed.size, self.params.investment_fraction * 100), self.datas[0].datetime.date(0))
 
-            self.positions_strategy[order.data._name] -= order.executed.size
+        elif order.issell():
+            self.add_log_entry('VENTA EJECUTADA CON ESTRATEGIA DE %s, ID: %i, ACTIVO: %s, PRECIO: %.2f, COSTO: %.2f, CANTIDAD: %i' % 
+                (str(self), order.ref, order.data._name, order.executed.price, order.executed.value, order.executed.size), self.datas[0].datetime.date(0))
+        else: return # Si bien en este contexto no debería darse, hay otras posibilidades además de isbuy() y issell(), donde no debería ejecutarse la última línea
+
+        self.assets_registry[order.data._name] += order.executed.size   # Tanto si era una orden de compra como si era de venta, se
+                                                                        # hace la suma (porque si era una orden de compra se compró
+                                                                        # algo y la posición debe aumentar, y si era de venta se
+                                                                        # vendió algo, pero el size era negativo por lo que termina siendo una resta de la cantidad de activos vendidos)
+
 
     def handle_not_completed_order(self, order):
         """
-        Para manejar órdenes no completadas debido a cancelaciones, rechazos o margen insuficiente.
+        Para manejar órdenes no completadas debido a cancelaciones, rechazos o margen insuficiente. Actualiza el fondo destinado
+        si se trataba de una orden de compra. Además, agrega logs para dichas órdenes.
 
         Args:
             order (Order): Objeto de orden no completada.
         """
         if order.isbuy():
-            amount_released = order.executed.size * order.data.close[0]
+            amount_released = order.size * order.data.close[0]
+            GeneralStrategy.pending_operation_funds -= amount_released # Lo calculo antes para mostrar el dinero que libera la operación en el log
             self.add_log_entry('ORDEN CANCELADA/MARGEN INSUFICIENTE/RECHAZADA, ID: %i, ACTIVO: %s, DINERO RESERVADO LIBERADO: %.2f' % 
-                (order.ref, order.data._name, amount_released))
-        else:
+                (order.ref, order.data._name, amount_released), self.datas[0].datetime.date(0))
+        elif order.issell():
             self.add_log_entry('ORDEN CANCELADA/MARGEN INSUFICIENTE/RECHAZADA, ID: %i, ACTIVO: %s' % 
-                (order.ref, order.data._name))
-            
+                (order.ref, order.data._name), self.datas[0].datetime.date(0))
+
     def notify_trade(self, trade):
         """
-        Este método notifica el estado de una operación cerrada y muestra las utilidades generadas, así como el valor de salida del portafolio tras la operación.
+        Este método notifica el estado de una operación cerrada y muestra el activo asociado, las utilidades generadas, así como el
+        valor de salida del portafolio tras la operación.
 
         Args:
             trade (Trade): Objeto de operación cerrada.
@@ -175,5 +196,5 @@ class GeneralStrategy(bt.Strategy):
             return
 
         portfolio_value = self.broker.get_value()
-        self.add_log_entry('OPERACION CERRADA, UTILIDAD: %.2f, VALOR DE SALIDA DEL PORTAFOLIO: %.2f' % 
-                (trade.pnl, portfolio_value))
+        self.add_log_entry('OPERACION CERRADA, ACTIVO: %s, UTILIDAD: %.2f, VALOR DE SALIDA DEL PORTAFOLIO: %.2f' % 
+                (trade.data._name, trade.pnl, portfolio_value), self.datas[0].datetime.date(0))
