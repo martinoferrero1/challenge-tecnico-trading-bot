72c626addee8481d1c92cd766fba5d3bdea9c92d
martinoferrero1 <martinoferrero02@gmail.com>
2024-12-16 17:18:45 -0300

    fix(cross_method): modificacion de la sma individual para incorporar un diccionario de smas, con una para cada datafeed de activo

--- Strategies/cross_method.py
+++ Strategies/cross_method.py
@@ -7,47 +7,57 @@ class CrossMethod(GeneralStrategy):
     para generar señales de compra y venta cuando el precio de cierre cruza la SMA.
 
     Atributos:
-        params (tuple): Parámetros de la estrategia, en este caso, el período de la SMA.
-        sma (bt.indicators.SimpleMovingAverage): Indicador SMA utilizado para calcular el promedio móvil simple.
+        smas (dict[bt.feeds.Data, bt.indicators.SimpleMovingAverage]): Indicadores SMA utilizados para cada datafeed.
 
     Args:
         period (int): El número de períodos para calcular la SMA, este posee un valor por defecto de 10.
     """
 
     params = (
-        ('period', 10)
+        ('period', 10),
     )
 
     def __init__(self):
         """
-        Para inicializar la estrategia y calcula la SMA utilizando el período proporcionado junto con el precio de cierre
+        Para inicializar la estrategia y calcula las SMAs utilizando el período proporcionado junto con el precio de cierre
         de los datos.
         """
-        self.sma = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.period)
+        super().__init__()
 
-    def conditions_buy(self):
+        self.smas = {}
+
+        for data in self.datas:
+            self.smas[data._name] = bt.indicators.SimpleMovingAverage(data.close, period=self.params.period)
+
+    def conditions_buy(self, data) -> bool:
         """
         Método que define las condiciones para ejecutar una orden de compra (implementación del método de la clase genérica de estrategia).
 
         La orden de compra se genera cuando el precio de cierre está por encima del valor de la SMA.
 
+        Args:
+            data (bt.feeds.YahooFinanceCSVData): Objeto de datos de Yahoo Finance con la información del activo correspondiente.
+
         Returns:
             bool: Verdadero si el precio de cierre es mayor que la SMA, lo que indica una señal de compra (por posible tendencia alcista).
         """
-        return self.data.close[0] > self.sma[0]
+        return data.close[0] > self.smas[data._name][0]
 
-    def conditions_sell(self):
+    def conditions_sell(self, data) -> bool:
         """
         Método que define las condiciones para ejecutar una orden de venta (implementación del método de la clase genérica de estrategia).
 
         La orden de venta se genera cuando el precio de cierre está por debajo del valor de la SMA.
 
+        Args:
+            data (bt.feeds.YahooFinanceCSVData): Objeto de datos de Yahoo Finance con la información del activo correspondiente.
+
         Returns:
             bool: Verdadero si el precio de cierre es menor que la SMA, lo que indica una señal de venta (ya que indica posible tendencia bajista).
         """
-        return self.data.close[0] < self.sma[0]
+        return data.close[0] < self.smas[data._name][0]
     
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Devuelve una representación en cadena de la estrategia, mostrando el nombre de la estrategia y el período de la SMA utilizada.
 
